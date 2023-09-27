f27c718711f2367a469a35a8ee7fba1f8941405e
XxCESARxX <cesar.amaya.gomes@gmail.com>
2023-09-27 13:20:26 -0500

    correccion de errores parte III

--- greeting/src/main/java/com/example/servingwebcontent/UserController.java
+++ greeting/src/main/java/com/example/servingwebcontent/UserController.java
@@ -1,32 +1,36 @@
 package com.example.servingwebcontent;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/hola")
+    @RequestMapping("/hello")
     @ResponseBody
-    private String hola() {
-        return "Hola mundo";
+    private String hello() {
+        return "Holiwi";
     }
-    @RequestMapping("/user /{id} ")
+    @RequestMapping("/user/{id}")
     @ResponseBody
-    private String getUser(@PathVariable Integer id) {
-        String uri = "https://jsonplaceholder.typicode.com/users/1";
+    private ModelAndView getUser(@PathVariable Integer id, Model model) {
+        String uri = "https://jsonplaceholder.typicode.com/users/" + id;
         RestTemplate restTemplate = new RestTemplate();
 
         User user = restTemplate.getForObject(uri, User.class);
-//        System.out.println("User: " + user);
-        System.out.println("Userid: " + user.getUserId());
-        System.out.println("id: " + user.getId());
-        System.out.println("title: " + user.getTitle());
-        System.out.println("complete: " + user.getComplete());
-        return "User detail page.";
+        ModelAndView modelAndView = new ModelAndView("user");
+
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("UserId", user.getUserId());
+        modelAndView.addObject("id", user.getId());
+        modelAndView.addObject("title", user.getTitle());
+        modelAndView.addObject("complete", user.getComplete());
+        return modelAndView;
     }
 }
